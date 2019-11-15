004009411aed8c616907c55c49a980afa864dd26
hubuntu <atyabi.hamid@yahoo.com>
2019-11-15 20:14:34 +0330

    initialize spring for authentication server microservice

---
+++ src/main/java/com/microservice/auth/AuthApplication.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2019.
+ * Developer: Hamid Atyabi
+ * Email: atyabi.hamid@yahoo.com
+ * Website: www.atyabi.com
+ */
+
+package com.microservice.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplication.class, args);
+	}
+
+}
